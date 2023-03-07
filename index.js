f9f1faef6883ea18bc517bd1d1154e35aa42409f
Abdullah Alsabi <abdullahb.alsabi@gmail.com>
2023-03-07 20:28:54 +0300

    FIX: Add CORS package

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3001;
 const tasksRouter = require("./routers/task_routes");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
